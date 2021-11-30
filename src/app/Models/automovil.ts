90e2a9316e6c50d8e6773d4f73fed7f58f3211bd
Juan David Neisa <jdneisap@correo.udistrital.edu.co>
2021-11-30 13:59:43 -0500

    Rama Develop. Cambios sobre variables no utilizadas

---
+++ src/app/Models/automovil.ts
@@ -0,0 +1,10 @@
+import { Time } from "@angular/common";
+
+export interface Automovil {
+    veh_placa : string,
+    veh_color : string,
+    veh_modelo : string,
+    fecha : Date,
+    hora : Time,
+    Tipo_Vehiculo_idTipo_Vehiculo : string
+}
